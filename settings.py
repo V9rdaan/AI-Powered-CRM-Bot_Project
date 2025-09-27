ca9ba3358ee1b14b7342902ce956f65859f9d36e
Vardaan <142111134+V9rdaan@users.noreply.github.com>
2025-09-27 15:09:19 +0530

    Refactor settings.py for environment variables
    Updated environment variable defaults for PostgreSQL and Redis settings.

--- settings.py
+++ settings.py
@@ -11,13 +11,14 @@ load_dotenv()
 # ====================
 # PostgreSQL Settings
 # ====================
-POSTGRES_HOST = os.getenv("DB_HOST", "localhost")
-POSTGRES_DB = os.getenv("DB_NAME", "CRMChatbot")
-POSTGRES_USER = os.getenv("DB_USER", "kartik")
-POSTGRES_PASSWORD = os.getenv("DB_PASS", "1234")
-POSTGRES_PORT = int(os.getenv("DB_PORT", 5432))
+POSTGRES_HOST = os.getenv("DB_HOST", "")
+POSTGRES_DB = os.getenv("DB_NAME", "")
+POSTGRES_USER = os.getenv("DB_USER", "")
+POSTGRES_PASSWORD = os.getenv("DB_PASS", "")
+POSTGRES_PORT = int(os.getenv("DB_PORT", ))
 
 # ====================
 # Redis Settings
 # ====================
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
