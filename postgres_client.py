2e18cd271c343e99d68f47bea7ac212e33299c73
Vardaan <142111134+V9rdaan@users.noreply.github.com>
2025-09-27 15:07:56 +0530

    Update postgres_client.py

--- postgres_client.py
+++ postgres_client.py
@@ -15,11 +15,11 @@ load_dotenv()
 def get_connection():
     """Get PostgreSQL connection"""
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "chat_bot"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD", "1234"),
-        port=os.getenv("DB_PORT", 5432)
+        host=os.getenv("DB_HOST", ""),
+        database=os.getenv("DB_NAME", ""),
+        user=os.getenv("DB_USER", "p"),
+        password=os.getenv("DB_PASSWORD", ""),
+        port=os.getenv("DB_PORT", )
     )
 
 def create_tables():
@@ -289,4 +289,5 @@ def update_user_stage(user_id: str, new_stage: str, client_id: str = "client_01"
         return False
 
 # Initialize tables when module is imported
+
 create_tables()
